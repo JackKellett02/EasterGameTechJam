b3bec21ad96e41800921b1ccc23d91c672194f92
Jack Kellett <jackkellett02@gmail.com>
2022-05-04 20:51:25 +0100

    Update PlayerPlatformer.cs

--- EasterGameTechnologiesJame/Assets/Scripts/Ludo/PlayerPlatformer.cs
+++ EasterGameTechnologiesJame/Assets/Scripts/Ludo/PlayerPlatformer.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.U2D.IK;
 using UnityEngine;
 
 public class PlayerPlatformer : MonoBehaviour {
